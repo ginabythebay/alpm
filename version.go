7b52d6adcbcf9ee7603347a7b4bddccb98290133
Gina White <ginabythebay@gmail.com>
2016-10-16 18:42:48 -0700

    first version of rpmVerCmp

--- version.go
+++ version.go
@@ -52,6 +52,214 @@ func parseEVR(text string) parsed {
 	return parsed{epoch, version, release}
 }
 
+type tokenType int
+
+const (
+	sepType     tokenType = iota
+	alphaType             // segment composed of letters
+	numericType           // segment composed of digits
+)
+
+type token struct {
+	kind tokenType
+	text []rune
+}
+
+func (t token) stripLeadingZeros() []rune {
+	for i, r := range t.text {
+		if r != '0' {
+			return t.text[i:]
+		}
+	}
+	return t.text[0:0]
+}
+
+func runeKind(r rune) tokenType {
+	switch {
+	case unicode.IsLetter(r):
+		return alphaType
+	case unicode.IsDigit(r):
+		return numericType
+	default:
+		return sepType
+	}
+}
+
+type separator int
+
+func nextSeparator(source []rune) (sep separator, remaining []rune) {
+	if len(source) == 0 {
+		return 0, source
+	}
+	for i, r := range source {
+		if runeKind(r) != sepType {
+			return separator(i), source[i:]
+		}
+	}
+	return separator(len(source)), source[0:0]
+}
+
+func (s separator) len() int {
+	return int(s)
+}
+
+func nextToken(source []rune) (t token, remaining []rune) {
+	if len(source) == 0 {
+		return token{sepType, source}, source
+	}
+	kind := runeKind(source[0])
+	for i, r := range source {
+		k := runeKind(r)
+		if k != kind {
+			return token{kind, source[:i]}, source[i:]
+		}
+	}
+	return token{kind, source}, source[0:0]
+}
+
+// like C strcmp, but for slices of runes
+func runeCmp(one, two []rune) int {
+	oneLen := len(one)
+	twoLen := len(two)
+	i := 0
+	for {
+		if i >= oneLen && i >= twoLen {
+			// We have compared all runes and both runs ended at the
+			// same time.
+			return 0
+		}
+		if i >= oneLen || i >= twoLen {
+			if twoLen > oneLen {
+				return 1
+			}
+			return -1
+		}
+
+		rOne := one[i]
+		rTwo := two[i]
+		if rOne < rTwo {
+			return 1
+		}
+		if rTwo < rOne {
+			return -1
+		}
+
+		i++
+	}
+}
+
+// rpmVerCmp compares epoch or version or release, deciding which is
+// newer, similar to rpmvercmp in version.c.
+//   0 if one and two are equal
+//  -1 if one is older
+//   1 if two is newer
+//
+// We expect to find segments (runs of letters or runs of digits)
+// possibly separated by separators, which are characters that are not
+// letters or digits.
+//
+// When comparing separators, we just compare length, with the longer
+// one being newer.
+//
+// Digit segments are always newer than letter segments or empty
+// segments.
+//
+// When comparing letter segments, we do the equivalent of C strcmp.
+//
+// When comparing digit segments, we strip leading zeros.  If the
+// lengths are different, then the longest one is newer.  Otherwise
+// treat as letter segment.
+//
+// If we have looped through all segments, we have special logic for
+// the final pair:
+//	 if one is empty and two is numeric, two is newer.
+//	 if one is an alpha, two is newer.
+//	 otherwise one is newer.
+func rpmVerCmp(one, two []rune) int {
+	for {
+		var sepOne, sepTwo separator
+		sepOne, one = nextSeparator(one)
+		sepTwo, two = nextSeparator(two)
+
+		// If we ran to the end of either, we are finished with the loop
+		if len(one) == 0 || len(two) == 0 {
+			break
+		}
+
+		// If the separator lengths were different, we are also finished
+		if sepOne.len() != sepTwo.len() {
+			if sepOne.len() < sepTwo.len() {
+				return 1
+			}
+			return -1
+		}
+
+		var tokOne, tokTwo token
+		tokOne, one = nextToken(one)
+		tokTwo, two = nextToken(two)
+
+		// this cannot happen, as we previously tested to make sure that
+		// the first string has a non-null segment
+		if tokOne.kind == sepType {
+			return -1 // arbitrary
+		}
+
+		// take care of the case where the two version segments are
+		// different types: one numeric, the other alpha.  Numeric
+		// segments are always newer than alpha segments XXX See patch
+		// #60884 (and details) from bugzilla #50977.
+		if tokOne.kind != tokTwo.kind {
+			if tokOne.kind == numericType {
+				return -1
+			}
+			return 1
+		}
+
+		if tokOne.kind == numericType {
+			// this used to be done by converting the digit segments
+			// to ints using atoi() - it's changed because long
+			// digit segments can overflow an int - this should fix that.
+
+			// throw away any leading zeros - it's a number, right?
+			tokOne.text = tokOne.stripLeadingZeros()
+			tokTwo.text = tokTwo.stripLeadingZeros()
+
+			// whichever number has more digits wins
+			if len(tokOne.text) < len(tokTwo.text) {
+				return 1
+			}
+			if len(tokTwo.text) < len(tokOne.text) {
+				return -1
+			}
+		}
+
+		rc := runeCmp(tokOne.text, tokTwo.text)
+		if rc != 0 {
+			return rc
+		}
+	}
+
+	// This catches the case where all numeric and alpha segments have
+	// compared identically and we ran out of more segments.
+	if len(one) == 0 || len(two) == 0 {
+		return 0
+	}
+
+	/* the final showdown. we never want a remaining alpha string to
+	 * beat an empty string. the logic is a bit weird, but:
+	 * - if one is empty and two is not an alpha, two is newer.
+	 * - if one is an alpha, two is newer.
+	 * - otherwise one is newer.
+	 * */
+	var tokOne, tokTwo token
+	tokOne, _ = nextToken(one)
+	tokTwo, _ = nextToken(two)
+	if (tokOne.kind == sepType && tokTwo.kind != alphaType) || tokOne.kind == alphaType {
+		return 1
+	}
+	return -1
+}
+
 // VerCmp compares two arch linux package version strings.  It returns:
 //   0 if the versions are equal
 //  -1 if a is older
--- version_test.go
+++ version_test.go
@@ -35,6 +35,99 @@ func TestParseEVR(t *testing.T) {
 	}
 }
 
+func rs(s string) []rune {
+	if len(s) == 0 {
+		return []rune{}
+	}
+	return []rune(s)
+}
+
+func TestNextSeprator(t *testing.T) {
+	allCases := []struct {
+		in        []rune
+		len       int
+		remaining []rune
+	}{
+		{rs("$$13"), 2, rs("13")},
+		{rs("$13"), 1, rs("13")},
+		{rs("13"), 0, rs("13")},
+		{rs(""), 0, rs("")},
+	}
+	for _, c := range allCases {
+		t.Run(string(c.in), func(t *testing.T) {
+			sep, remaining := nextSeparator(c.in)
+			equals(t, c.len, sep.len())
+			equals(t, c.remaining, remaining)
+		})
+	}
+}
+
+func TestNextToken(t *testing.T) {
+	allCases := []struct {
+		in        []rune
+		tok       token
+		remaining []rune
+	}{
+		{rs("13"), token{numericType, rs("13")}, rs("")},
+		{rs(""), token{sepType, rs("")}, rs("")},
+
+		{rs("13ab"), token{numericType, rs("13")}, rs("ab")},
+		{rs("ab"), token{alphaType, rs("ab")}, rs("")},
+	}
+	for _, c := range allCases {
+		t.Run(string(c.in), func(t *testing.T) {
+			tok, remaining := nextToken(c.in)
+			equals(t, c.tok, tok)
+			equals(t, c.remaining, remaining)
+		})
+	}
+}
+
+func TestRpmVerCmp(t *testing.T) {
+	allCases := []struct {
+		one      string
+		two      string
+		expected int
+	}{
+		// test comparison of just two numbers
+		{"1", "2", 1},
+		{"2", "1", -1},
+		{"2", "2", 0},
+		{"02", "2", 0},
+		{"02", "2", 0},
+		{"020", "2", -1},
+
+		// test comparison of just alpha types
+		{"a", "b", 1},
+		{"b", "a", -1},
+		{"b", "b", 0},
+		{"b", "ba", 1},
+
+		// test comparison of just separators
+		{"$a", "#b", 1},
+		{"$b", "#a", -1},
+		{"$$a", "#a", -1},
+		{"$a", "##a", 1},
+
+		// test numeric vs. alpha
+		{"1", "a", -1},
+		{"a", "1", 1},
+
+		// test a few composite cases
+		{"1a01", "1a2", 1},
+		{"1a1", "1a2", 1},
+		{"1a$1", "1a$2", 1},
+		{"1a$$1", "1a$2", -1},
+	}
+
+	for _, c := range allCases {
+		t.Run(fmt.Sprintf("%q:%q", c.one, c.two), func(t *testing.T) {
+			actual := rpmVerCmp(rs(c.one), rs(c.two))
+			equals(t, c.expected, actual)
+		})
+	}
+}
+
 // equals fails the test if exp is not equal to act.
 func equals(tb testing.TB, exp, act interface{}) {
 	if !reflect.DeepEqual(exp, act) {
